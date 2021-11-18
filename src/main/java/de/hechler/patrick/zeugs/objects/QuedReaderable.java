91bf0024752f356fc6b52ff108feb0f1976dbb0c
Patrick <Patrick@Coruscant.speedport.ip>
2021-11-18 15:35:38 +0100

    QuedReaderable can now append Readable s

--- src/main/java/de/hechler/patrick/zeugs/objects/QuedReaderable.java
+++ src/main/java/de/hechler/patrick/zeugs/objects/QuedReaderable.java
@@ -7,8 +7,8 @@ import java.util.Queue;
 
 public class QuedReaderable implements Readable, Appendable {
 	
-	private final Queue <char[]> buffer;
-	private int timeout;
+	private final Queue <Object> buffer;
+	private int                  timeout;
 	
 	
 	
@@ -16,9 +16,27 @@ public class QuedReaderable implements Readable, Appendable {
 		this(new QueueImpl <>(), 0);
 	}
 	
-	public QuedReaderable(Queue <char[]> que, int timeout) {
+	public QuedReaderable(int timeout, String... startelements) {
+		this(new QueueImpl <>(), timeout, (Object[]) startelements);
+	}
+	
+	public QuedReaderable(int timeout, char[]... startelements) {
+		this(new QueueImpl <>(), timeout, (Object[]) startelements);
+	}
+	
+	public QuedReaderable(int timeout, Readable... startelements) {
+		this(new QueueImpl <>(), timeout, (Object[]) startelements);
+	}
+	
+	private QuedReaderable(Queue <Object> que, int timeout, Object... startelements) {
 		this.buffer = que;
 		this.timeout = timeout;
+		for (Object element : startelements) {
+			if (element.getClass() == String.class) {
+				element = ((String) element).toCharArray();
+			}
+			this.buffer.add(element);
+		}
 	}
 	
 	
@@ -28,9 +46,12 @@ public class QuedReaderable implements Readable, Appendable {
 	}
 	
 	/**
-	 * set the timeout in ms to wait at most when there
+	 * set the timeout in ms to wait at most when there<br>
+	 * 
+	 * set to zero to wait without timeout and -1 to not wait any time
 	 * 
 	 * @param timeout
+	 *            the timeout in ms
 	 */
 	public void setTimeout(int timeout) {
 		if (timeout < 0 && timeout != -1) {
@@ -81,6 +102,17 @@ public class QuedReaderable implements Readable, Appendable {
 		return true;
 	}
 	
+	public boolean append(Readable reader) {
+		if (timeout == -2) {
+			throw new IllegalStateException("end was already appended!");
+		}
+		synchronized (this) {
+			buffer.add(reader);
+			this.notifyAll();
+		}
+		return true;
+	}
+	
 	public void appendEnd() {
 		synchronized (this) {
 			this.timeout = -2;
@@ -88,24 +120,52 @@ public class QuedReaderable implements Readable, Appendable {
 		}
 	}
 	
+	public boolean hasEndAppended() {
+		return timeout == -2;
+	}
+	
+	public boolean finished() {
+		return timeout == -2 && buffer.isEmpty();
+	}
+	
+	public boolean canRead() {
+		return !buffer.isEmpty();
+	}
+	
 	@Override
 	public int read(CharBuffer cb) throws IOException {
-		char[] chars;
 		synchronized (this) {
-			if (timeout >= 0 && buffer.isEmpty()) {
-				try {
-					this.wait(timeout);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
+			while (true) {
+				Object element;
+				if (timeout >= 0 && buffer.isEmpty()) {
+					try {
+						this.wait(timeout);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				element = buffer.peek();
+				if (element == null) {
+					return -1;
+				}
+				if (element.getClass() == char[].class) {
+					buffer.remove();
+					cb.put((char[]) element);
+					return ((char[]) element).length;
+				} else if (element instanceof Readable) {
+					Readable r = (Readable) element;
+					int read = r.read(cb);
+					if (read == -1) {
+						buffer.remove();
+						continue;
+					} else {
+						return read;
+					}
+				} else {
+					throw new InternalError("unknown object in que: class: " + element.getClass().getCanonicalName() + " toString()->'" + element.toString() + "'");
 				}
 			}
-			chars = buffer.remove();
 		}
-		if (chars == null) {
-			return -1;
-		}
-		cb.put(chars);
-		return chars.length;
 	}
 	
 }
