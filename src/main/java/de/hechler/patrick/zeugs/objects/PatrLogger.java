bcced883e4050cb4665815aaf21a23241e761f47
Patrick <Patrick@Coruscant.speedport.ip>
2022-01-20 20:31:12 +0100

    update

--- src/main/java/de/hechler/patrick/zeugs/objects/PatrLogger.java
+++ src/main/java/de/hechler/patrick/zeugs/objects/PatrLogger.java
@@ -3,44 +3,42 @@ package de.hechler.patrick.zeugs.objects;
 import java.io.Closeable;
 import java.io.PrintStream;
 import java.io.Serializable;
-import java.lang.System.Logger;
 import java.util.Collections;
 import java.util.List;
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
+import java.util.logging.Filter;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
-public class PatrLogger implements Closeable, Logger, Serializable {
+public class PatrLogger extends Logger implements Closeable, Serializable {
 	
 	/** UID */
 	private static final long serialVersionUID = -7744521531573940412L;
 	
 	private final PrintStream out;
 	private final String name;
-	private final String postfix;
-	private final String childadd;
+	private final String resourceBundleName;
+	private final String lineSep;
 	private final PatrLogger parent;
 	private List <PatrLogger> childs;
-	private Level loglevel;
 	
 	public PatrLogger(PrintStream out, String name) {
-		this(out, name, "\n", ">", null, Level.ALL);
+		this(out, name, null, "\n", null);
 	}
 	
-	public PatrLogger(PrintStream out, String name, String postfix) {
-		this(out, name, postfix, ">", null, Level.ALL);
+	public PatrLogger(PrintStream out, String name, String resourceBundleName, String lineSep) {
+		this(out, name, resourceBundleName, lineSep, null);
 	}
 	
-	public PatrLogger(PrintStream out, String name, String postfix, String childadd, Level loglevel) {
-		this(out, name, postfix, childadd, null, Level.ALL);
-	}
-	
-	private PatrLogger(PrintStream out, String name, String postfix, String childadd, PatrLogger parent, Level loglevel) {
+	private PatrLogger(PrintStream out, String name, String resourceBundleName, String lineSep, PatrLogger parent) {
+		super(name, resourceBundleName);
 		this.out = out;
 		this.name = name;
-		this.postfix = (postfix.contains("\r") || postfix.contains("\n")) ? postfix : (postfix.concat(System.lineSeparator()));
-		this.childadd = childadd;
+		this.resourceBundleName = resourceBundleName;
+		this.lineSep = (lineSep.contains("\r") || lineSep.contains("\n")) ? lineSep : (lineSep.concat(System.lineSeparator()));
 		this.parent = parent;
 		this.childs = new ArrayListImpl <>(PatrLogger.class);
-		this.loglevel = loglevel;
 	}
 	
 	
@@ -49,72 +47,137 @@ public class PatrLogger implements Closeable, Logger, Serializable {
 		return name;
 	}
 	
+	// @Override
+	// public void log(Level level, ResourceBundle bundle, String msg, Throwable thrown) {
+	// if (!isLoggable(level)) {
+	// return;
+	// }
+	// if (thrown != null) {
+	// log(new String[] {"error:" });
+	// log(thrown);
+	// }
+	// if (bundle != null) {
+	// Object obj = bundle.getObject(msg);
+	// if (obj instanceof String) {
+	// log((String) obj);
+	// } else if (obj instanceof String[]) {
+	// for (String potline : (String[]) obj) {
+	// log(potline);
+	// }
+	// } else {
+	// throw new InternalError("the ResourceBundle did not return a String or a String[]: class: " +
+	// obj.getClass() + " tos: '" + obj + "'");
+	// }
+	// } else {
+	// log(msg);
+	// }
+	// }
+	//
+	// @Override
+	// public void log(Level level, ResourceBundle bundle, String format, Object... params) {
+	// if (!isLoggable(level)) {
+	// return;
+	// }
+	// String msg = String.format(format, params);
+	// if (bundle != null) {
+	// Object obj = bundle.getObject(msg);
+	// if (obj instanceof String) {
+	// log((String) obj);
+	// } else if (obj instanceof String[]) {
+	// for (String potline : (String[]) obj) {
+	// log(potline);
+	// }
+	// } else {
+	// throw new InternalError("the ResourceBundle did not return a String or a String[]: class: " +
+	// obj.getClass() + " tos: '" + obj + "'");
+	// }
+	// } else {
+	// log(msg);
+	// }
+	// }
+	
 	@Override
-	public boolean isLoggable(Level level) {
-		return level.getSeverity() >= this.loglevel.getSeverity();
+	public void setParent(Logger parent) {
+		// only allow init
+		if (this.parent != super.getParent() && parent == this.parent) {
+			super.setParent(parent);
+		}
+		throw new UnsupportedOperationException("setParent(parent) parent=" + parent);
 	}
 	
 	@Override
-	public void log(Level level, ResourceBundle bundle, String msg, Throwable thrown) {
-		if (level.getSeverity() < this.loglevel.getSeverity()) {
-			return;
-		}
-		if (thrown != null) {
-			log(new String[] {"error:" });
-			log(thrown);
-		}
-		if (bundle != null) {
-			Object obj = bundle.getObject(msg);
-			if (obj instanceof String) {
-				log((String) obj);
-			} else if (obj instanceof String[]) {
-				for (String potline : (String[]) obj) {
-					log(potline);
-				}
-			} else {
-				throw new InternalError("the ResourceBundle did not return a String or a String[]: class: " + obj.getClass() + " tos: '" + obj + "'");
-			}
-		} else {
-			log(msg);
-		}
+	public PatrLogger getParent() {
+		return parent;
 	}
 	
 	@Override
-	public void log(Level level, ResourceBundle bundle, String format, Object... params) {
-		if (level.getSeverity() < this.loglevel.getSeverity()) {
+	public void log(LogRecord record) {
+		if ( !isLoggable(record.getLevel())) {
 			return;
 		}
-		String msg = String.format(format, params);
-		if (bundle != null) {
-			Object obj = bundle.getObject(msg);
-			if (obj instanceof String) {
-				log((String) obj);
-			} else if (obj instanceof String[]) {
-				for (String potline : (String[]) obj) {
-					log(potline);
-				}
-			} else {
-				throw new InternalError("the ResourceBundle did not return a String or a String[]: class: " + obj.getClass() + " tos: '" + obj + "'");
-			}
-		} else {
-			log(msg);
+		Filter f = super.getFilter();
+		if (f != null && !f.isLoggable(record)) {
+			return;
 		}
+		String msg = record.getMessage();
+		Object[] params = record.getParameters();
+		ResourceBundle rb = record.getResourceBundle();
+		log(rb, msg, params);
+		Throwable error = record.getThrown();
+		if (error != null) {
+			log("  error:");
+			log(error, "  ");
+		}
+		super.log(record);
+	}
+	
+	public void log(ResourceBundle rb, String msg, Object[] params) throws ClassCastException {
+		msg = msg == null ? "" : msg;
+		params = params == null ? new Object[0] : params;
+		Object replacingmsg = null;
+		if (rb != null) {
+			try {
+				replacingmsg = rb.getObject(msg);
+			} catch (MissingResourceException e) {
+			}
+		}
+		if (replacingmsg != null) {
+			if (replacingmsg instanceof String) {
+				msg = (String) replacingmsg;
+			} else if (replacingmsg instanceof String[]) {
+				String[] arr = (String[]) replacingmsg;
+				StringBuilder build = new StringBuilder();
+				for (String str : arr) {
+					build.append(str).append(lineSep);
+				}
+				msg = build.toString();
+			} else {
+				throw new ClassCastException("cannt cas from " + replacingmsg.getClass().getName() + " to String or String[]");
+			}
+		}
+		msg = String.format(msg, params);
+		log(msg);
 	}
 	
 	public void log(Throwable error) {
-		log("  cls: " + error.getClass().getCanonicalName());
-		log("  msg: " + error.getMessage());
+		log(error, "");
+	}
+	
+	public void log(Throwable error, String indention) {
+		log(indention + "cls: " + error.getClass().getCanonicalName());
+		log(indention + "msg: " + error.getMessage());
+		String subIndention = indention + "  ";
 		for (StackTraceElement traceElement : error.getStackTrace()) {
-			log("    at " + traceElement);
+			log(subIndention + "at " + traceElement);
 		}
 		for (Throwable suppressed : error.getSuppressed()) {
-			log("  suppressed:");
-			log(suppressed);
+			log(indention + "suppressed:");
+			log(suppressed, subIndention);
 		}
 		Throwable cause = error.getCause();
 		if (cause != null) {
-			log("  cause:");
-			log(cause);
+			log(indention + "cause:");
+			log(cause, subIndention);
 		}
 	}
 	
@@ -124,13 +187,10 @@ public class PatrLogger implements Closeable, Logger, Serializable {
 	
 	private void log(String[] lines) {
 		for (String line : lines) {
-			out.print("[" + name + "]:" + line + postfix);
+			out.print("[" + name + "]:" + line + lineSep);
 		}
 	}
 	
-	public PatrLogger getParent() {
-		return parent;
-	}
 	
 	public List <PatrLogger> getChilds() {
 		return Collections.unmodifiableList(childs);
@@ -140,16 +200,17 @@ public class PatrLogger implements Closeable, Logger, Serializable {
 		if (parent == null) {
 			return new PatrLogger(out, sname);
 		} else {
-			return parent.addChild(out, sname, postfix, childadd, loglevel);
+			return parent.addChild(out, sname, lineSep);
 		}
 	}
 	
 	public PatrLogger addChild(String cname) {
-		return addChild(out, cname, postfix, childadd, loglevel);
+		return addChild(out, cname, lineSep);
 	}
 	
-	public PatrLogger addChild(PrintStream out, String name, String postfix, String childadd, Level loglevel) {
-		PatrLogger c = new PatrLogger(out, this.name + this.childadd + name, postfix, childadd, loglevel);
+	public PatrLogger addChild(PrintStream out, String name, String postfix) {
+		PatrLogger c = new PatrLogger(out, this.name + '.' + name, resourceBundleName, postfix);
+		c.setParent(this);
 		this.childs.add(c);
 		return c;
 	}
@@ -168,7 +229,7 @@ public class PatrLogger implements Closeable, Logger, Serializable {
 	
 	@Override
 	public String toString() {
-		return "PatrLogger [name=" + name + ", postfix=" + postfix + ", childadd=" + childadd + ", childs=" + childs + ", loglevel=" + loglevel + ", out=" + out + "]";
+		return "PatrLogger [name=" + name + ", postfix=" + lineSep + ", childs=" + childs + ", out=" + out + "]";
 	}
 	
 }
